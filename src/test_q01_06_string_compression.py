083d60c71f31be3b757daefd64524e2e76fc2ae5
Sean.Wu <srw.leaven@gmail.com>
2021-07-31 17:52:11 +0800

    optimize solution on q1.6

--- src/test_q01_06_string_compression.py
+++ src/test_q01_06_string_compression.py
@@ -1,23 +1,41 @@
 
+### initial solution
+# def str_compression(s):
+#     if len(s) == 0:
+#         return s
 
+#     out = s[0]
+#     letter, count = s[0], 1
+#     for i in range(1, len(s)):
+#         if s[i] == letter:
+#             count += 1
+#         else:
+#             letter = s[i]
+#             out += str(count) + s[i]
+#             count = 1
+#     out += str(count)
+
+#     print(out)
+#     return out if len(out) < len(s) else s
+
+### solution with avoiding string concatenation
 def str_compression(s):
     if len(s) == 0:
         return s
 
-    out = s[0]
+    out = [s[0]]
     letter, count = s[0], 1
     for i in range(1, len(s)):
         if s[i] == letter:
             count += 1
         else:
             letter = s[i]
-            out += str(count) + s[i]
+            out.extend((str(count), s[i]))
             count = 1
-    out += str(count)
+    out.append(str(count))
 
     print(out)
-    return out if len(out) < len(s) else s
-
+    return ''.join(out) if len(out) < len(s) else s
 
 
 def test_func():
