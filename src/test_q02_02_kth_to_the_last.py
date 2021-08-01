3fa97d7bb87658f6626e8842f25f155d759a5416
Sean.Wu <srw.leaven@gmail.com>
2021-08-01 15:05:27 +0800

    Add recursive solution to q2.2

--- src/test_q02_02_kth_to_the_last.py
+++ src/test_q02_02_kth_to_the_last.py
@@ -1,18 +1,33 @@
 
 from linkedlist import LinkedList
 
-def kth_to_the_last(l, n):
-    head = l.head
-    fast = head
-    slow = None
-    while fast != None:
-        fast = fast.next
-        if n > 0 :
-            n -= 1
-        elif n == 0:
-            slow = slow.next if slow != None else head
-    return slow.data if slow != None else None
+# two-runner solution
+# def kth_to_the_last(l, n):
+#     head = l.head
+#     fast = head
+#     slow = None
+#     while fast != None:
+#         fast = fast.next
+#         if n > 0 :
+#             n -= 1
+#         elif n == 0:
+#             slow = slow.next if slow != None else head
+#     return slow.data if slow != None else None
 
+# recursive solution
+def helper(node, n, ans):
+    if node == None:
+        return -1
+    cnt = helper(node.next, n, ans) + 1
+    if cnt == n:
+        ans.append(node.data)
+    return cnt
+
+def kth_to_the_last(l, n):
+    node = l.head
+    ans = []
+    helper(node, n, ans)
+    return ans[0] if len(ans) != 0 else None
 
 def test_func():
     assert kth_to_the_last(LinkedList([1,2,3,4,5,6]), 0) == 6
