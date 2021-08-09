75b202e58b7154e74fbd20901eafce8964cf84c1
Sean Wu <43980497+SeanWuLearner@users.noreply.github.com>
2021-08-09 09:58:55 +0000

    Add book solution to q2.7

--- src/test_q02_07_intersection.py
+++ src/test_q02_07_intersection.py
@@ -1,23 +1,60 @@
 
 from linkedlist import LinkedList, Node
 
+# Solution1: idea len(l1 + l2) = len(l2 + l1), they will reach the intersection on 2nd run.
+# def find_intersection(l1, l2):
+#     n1 = l1.head
+#     n2 = l2.head
+#     if n1 == None or n2 == None:
+#         return False
+
+#     while True:
+#         if n1 is n2:
+#             return n1
+
+#         # leave here if they have no intersection (reach the end at the same time)
+#         if n1.next == None and n2.next==None:
+#             return None
+
+#         n1 = n1.next if n1.next != None else l2.head
+#         n2 = n2.next if n2.next != None else l1.head
+
+
+# Solution 2: Figure out len diff on both list in 1st pass. On 2nd pass,
+#             elminate the diff to make them arrive the intersection together.
+def find_len(node):
+    length = 0
+    while node != None:
+        length += 1
+        node = node.next
+    return length
 
 def find_intersection(l1, l2):
     n1 = l1.head
     n2 = l2.head
-    if n1 == None or n2 == None:
-        return False
 
-    while True:
-        if n1 is n2:
-            return n1
+    len1 = find_len(n1)
+    len2 = find_len(n2)
 
-        # leave here if they have no intersection (reach the end at the same time)
-        if n1.next == None and n2.next==None:
-            return None
+    if len1 > len2:
+        longer, shorter = n1, n2
+    else:
+        longer, shorter = n2, n1
 
-        n1 = n1.next if n1.next != None else l2.head
-        n2 = n2.next if n2.next != None else l1.head
+    len_diff = abs(len1 - len2)
+
+    # step1: forward longer list with len_diff nodes
+    while len_diff != 0:
+        longer = longer.next
+        len_diff -= 1
+
+    #step2: proceed two list together
+    while longer != None or shorter != None:
+        if longer is shorter:
+            return longer
+        longer = longer.next
+        shorter = shorter.next
+    return None
 
 
 def test_func():
